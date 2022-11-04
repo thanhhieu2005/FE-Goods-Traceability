1b414f040b3f4c7617286a4024fd593307bedebd
Le Ho Hai Duong <lehohaiduong2001@gmail.com>
2022-11-04 10:21:59 +0700

    feat: config eslint

--- src/utils/validation/Validation.ts
+++ src/utils/validation/Validation.ts
@@ -1,21 +1,22 @@
-import { isFulfilled } from "@reduxjs/toolkit";
+import { isFulfilled } from '@reduxjs/toolkit';
 
-export const isEmty = value => {
-    if(!value) return true;
-    return false;
-}
+export const isEmty = (value: unknown) => {
+  if (!value) return true;
+  return false;
+};
 
-export const isEmail = email => {
-    const  re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    return re.test(email);
-}
+export const isEmail = (email: string) => {
+  const re =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return re.test(email);
+};
 
-export const isPassword = password => {
-    const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/i;
-    return re.test(password);
-}
+export const isPassword = (password: string) => {
+  const re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/i;
+  return re.test(password);
+};
 
-export const isMatch = (password, confirmPassword) => {
-    if(password === confirmPassword) return true;
-    return false;
-}
+export const isMatch = (password: string, confirmPassword: string) => {
+  if (password === confirmPassword) return true;
+  return false;
+};
